93d55adb8ffa537884d5fe0698ce41a816ce0659
AnibalF <anibalgarciapreu@gmail.com>
2022-06-13 15:48:04 -0600

    CreacionDeRegistro

---
+++ registrar.php
@@ -0,0 +1,13 @@
+<?php 
+include("conexion.php");
+$con = conectar();
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$telefono = $_POST['telefono'];
+
+$sql ="INSERT INTO usuario(nombre,apellido,telefono) VALUES('$nombre','$apellido','$telefono')";
+if(($resultado = mysqli_query($con,$sql))===false){
+    die(mysqli_error($con));
+}
+    header("refresh:2;url=http://localhost/template/consulta.php");
+?>
